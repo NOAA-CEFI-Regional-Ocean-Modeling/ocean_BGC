e75d9fb4d5c607a37277c23c26cf3bc72efe8f35
Andrew Ross <5852283+andrew-c-ross@users.noreply.github.com>
2024-08-21 14:25:59 -0400

    jo2 is layer integral, not concentration

--- generic_tracers/cobalt_reg_diag.F90
+++ generic_tracers/cobalt_reg_diag.F90
@@ -2664,7 +2664,7 @@ module COBALT_reg_diag
     cobalt%id_jo2_plus_btm = register_diag_field(package_name, vardesc_temp%name, axes(1:3),&
          init_time, vardesc_temp%longname,vardesc_temp%units, missing_value = missing_value1)
 
-    vardesc_temp = vardesc("jo2","O2 source concentration",'h','L','s','mol m-2 s-1','f')
+    vardesc_temp = vardesc("jo2","O2 source concentration layer integral",'h','L','s','mol m-2 s-1','f')
     cobalt%id_jo2 = register_diag_field(package_name, vardesc_temp%name, axes(1:3),&
          init_time, vardesc_temp%longname,vardesc_temp%units, missing_value = missing_value1)
 
