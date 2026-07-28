15e766dfe692537c55bc120b633e2fcd1e8c3ebc
Jessica Luo <jessluo@gmail.com>
2026-07-28 15:52:08 -0400

    clean up jmetabo diagnostics, pass dvm zooplankton jmetabo_n to g_send_data. bug found by claude opus 5.

--- generic_tracers/cobalt_send_diag.F90
+++ generic_tracers/cobalt_send_diag.F90
@@ -1607,6 +1607,12 @@ module COBALT_send_diag
               model_time, rmask = grid_tmask, is_in=isc, js_in=jsc, ks_in=1,ie_in=iec, je_in=jec, ke_in=nk)
             used = g_send_data(zoo(n)%id_temp_lim, zoo(n)%temp_lim, &
               model_time, rmask = grid_tmask, is_in=isc, js_in=jsc, ks_in=1,ie_in=iec, je_in=jec, ke_in=nk)
+            ! jmetabo_n is allocated, and id_jmetabo_n registered, only for the vertically migrating
+            ! groups (see user_allocate_arrays and cobalt_reg_diagnostics)
+            if ( n .eq. 4 .or. n .eq. 5 ) then !{
+              used = g_send_data(zoo(n)%id_jmetabo_n, zoo(n)%jmetabo_n, &
+                model_time, rmask = grid_tmask, is_in=isc, js_in=jsc, ks_in=1,ie_in=iec, je_in=jec, ke_in=nk)
+            endif !}
           enddo
 
           !
